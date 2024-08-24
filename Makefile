c9f9d298d1818aafa5d1c99941ccf7f64b0c94a1
aabysswalker <levydevelope@gmail.com>
2024-08-24 20:10:40 +0300

    1 task

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 %:
 	nasm -f elf -o solution$@/task$@.o solution$@/task$@.asm
 	ld -m elf_i386 -o solution$@/task$@ solution$@/task$@.o
+	./solution$@/task$@
--- solution1/task1.asm
+++ solution1/task1.asm
@@ -1,8 +1,18 @@
+section .bss
+    res resd 1
 section .data
+    fnum dd -1123492
+    snum dd 2534232
+    tnum dd -3123442
+
 section .text
-global _start
+    global _start   
 
 _start:
-    mov eax,1
-    mov ebx,1
-    int 80h
+    mov eax, [fnum]
+    add eax, [snum]
+    add eax, [tnum]
+    mov [res], eax
+    mov eax, 1
+    mov ebx, 0
+    int 0x80  

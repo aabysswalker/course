3f15e6ae31b750f0cc12004f7fd489acdebd8ede
aabysswalker <levydevelope@gmail.com>
2024-08-25 18:01:16 +0300

    3-4 tasks

--- solution3/task3.asm
+++ solution3/task3.asm
@@ -1,1 +1,25 @@
-// Sample
+section .bss
+    result resd 1
+section .data
+    fnum dd -21234
+    snum dd -11234
+    tnum dd -31234
+
+section .text
+    global _start
+
+_start:
+    mov eax, [snum]
+    mov edx, [fnum]
+    cmp eax, edx
+    cmovl eax, edx
+
+    mov edx, [tnum]
+    cmp eax, edx
+    cmovl eax, edx
+
+    mov [result], eax
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
--- solution4/task4.asm
+++ solution4/task4.asm
@@ -1,1 +1,25 @@
-// Sample
+section .data
+    array dd 1,2,3,4,4,3,2,1
+    array_length equ 8
+
+section .bss
+    result resd 1
+
+section .text
+    global _start
+
+_start:
+    mov eax, [array]
+    mov esi, array + 4
+    mov ecx, array_length
+_findmax:
+    cmp eax, [esi]
+    cmovl eax, [esi]
+    add esi, 4
+    loop _findmax
+
+    mov [result], eax
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80

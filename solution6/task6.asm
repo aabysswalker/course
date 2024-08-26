b00afbfca47dccf7401a86a05ed2c580f0a2723b
aabysswalker <levydevelope@gmail.com>
2024-08-26 15:02:12 +0300

    scanf and printf implementation

--- solution6/task6.asm
+++ solution6/task6.asm
@@ -1,1 +1,94 @@
-// Sample
+section .bss
+    buffer_write resb 16
+    buffer_read resb 16
+    fnumber resd 1
+section .text
+    global _start
+
+_start:
+    call scanf    
+    mov [fnumber], eax
+    mov eax, [fnumber]
+    call printf
+
+    exit:
+        mov eax, 1
+        xor ebx, ebx
+        int 0x80
+
+printf:
+    test eax, eax
+    jge _store_number
+    mov byte [buffer_read], '-'
+    neg eax
+    mov esi, buffer_read
+    inc esi
+    jmp _convert_number
+
+    _store_number:
+        mov esi, buffer_read
+
+    _convert_number:
+        xor ecx, ecx
+        mov ebx, 10
+
+    _convert_loop:
+        xor edx, edx
+        div ebx
+        add dl, '0'
+        push dx
+        inc ecx
+        test eax, eax
+        jnz _convert_loop
+
+        _print_digits:
+            pop dx
+            mov [esi], dl
+            inc esi
+            loop _print_digits
+
+        mov byte [esi], 0x0A
+        inc esi
+
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, buffer_read
+        mov edx, 16
+        int 0x80
+        ret
+
+scanf:
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, buffer_write
+    mov edx, 16
+    int 0x80
+
+    xor eax, eax
+    mov esi, buffer_write
+    mov dl, [esi]
+    test dl, dl
+    jz _convert_char
+    cmp dl, '-'
+    jne _convert_char
+    inc esi
+    mov edi, -1
+
+    _convert_char:
+        movzx edx, byte [esi]
+        cmp edx, 0x0A
+        je _done
+        imul eax, 10
+        sub edx, '0'
+        add eax, edx
+        inc esi
+        jmp _convert_char
+
+    _done:
+        test edi, edi
+        jnz _negate
+        ret
+
+    _negate:
+        neg eax    
+        ret
